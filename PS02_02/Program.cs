75c9678aa93885068c0e872780b4a14194519d6c
Reymart Deola Cruz <rd.cruz1101@gmail.com>
2024-08-21 12:03:40 +0800

    Code for odd-numbers
    Prints odd numbers from 1-20.

--- PS02_02/Program.cs
+++ PS02_02/Program.cs
@@ -10,9 +10,16 @@ namespace PS02_02
     {
         static void Main(string[] args)
         {
-            /*
-            * Write a program that prints all the odd numbers from 1 to 20 using a for loop. 
-            */
+            int num = 1;
+            for (int i=0; i<20; i++)
+            {
+                if (num % 2 != 0)
+                {
+                    Console.WriteLine(num);
+                }
+                num++;
+            }
+            Console.ReadKey();
         }
     }
 }
