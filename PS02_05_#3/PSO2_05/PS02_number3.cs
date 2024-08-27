f302c5cf3b010febc417c4655da1e10e573f2dec
Iskatiboi <scottfranklinmaher@gmail.com>
2024-08-27 22:22:29 +0800

    PS02_05

---
+++ PS02_05_#3/PSO2_05/PS02_number3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PSO2_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a character: ");
+            char letter = Convert.ToChar(Console.ReadLine());
+
+            if (letter == 'a' || letter == 'e' || letter == 'i'   || letter == 'o' || letter == 'u')
+            {
+              Console.WriteLine("The letter " +letter+ " is a vowel ");
+            }
+            else
+            {
+              Console.WriteLine("The letter " +letter + " is a consonant");
+            }
+        }
+    }
+}
